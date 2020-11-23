9c82acd34665f70611d73061416531d4c7dd151e
吳哲鐘 <>
2020-11-24 01:10:53 +0800

    1.新增GetShipper API

--- Northwind.Core/Controllers/ApiController.cs
+++ Northwind.Core/Controllers/ApiController.cs
@@ -1,11 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Core.Repository.DTOs;
+using Northwind.Core.Services.Interface;
 
 namespace Northwind.Core.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/shipper/[action]/")]
     public class ApiController : Controller
     {
-        
+        private readonly IShipperService ShipperService;
+
+        public ApiController(IShipperService shipperService)
+        {
+            this.ShipperService = shipperService;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public Response<Shipper> GetShipper(string id)
+        {
+            int.TryParse(id, out int sid);
+            return this.ShipperService.GetShipperById(sid);
+        }
     }
 }
